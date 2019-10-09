7cdcef906d3eb93a09d7dd204dc2050c532fc91f
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-09 20:10:20 +0200

    done

---
+++ ProsteDodawanie/ProsteDodawanie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ProsteDodawanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int numbers_count = int.Parse(Console.ReadLine());
+                string text = Console.ReadLine();
+                string[] text_tab = text.Split(' ');
+                int sum = 0;
+                for (int j = 0; j < text_tab.Length; j++)
+                {
+                    sum += int.Parse(text_tab[j]);
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            int test_count = 0;
+            for (int i = 0; i < test_count; i++)
+            {
+                int value = Console.Read();
+                string result;
+                if (IsPrimaryNumber(value)) Console.WriteLine("TAK"); else Console.WriteLine("NIE");
+
+
+            }
+
+            System.Console.ReadLine(); 
+            
+        }
+
+        public static bool IsPrimaryNumber(int value)
+        {
+            for (int i = 2; i < value/2; i++)
+            {
+                if (value % i == 0) return false;
+            }  
+            return true;
+        }
+    }
+
+
+}
