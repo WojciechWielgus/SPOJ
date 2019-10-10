fef46f147ee3b29f8e1fe2681fe89c0538996829
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-10 16:47:49 +0200

    done

---
+++ 496_Dwie_cyfry_silni/496_Dwie_cyfry_silni/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _496_Dwie_cyfry_silni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x < 10)
+                {
+                    int sum = 1;
+                    for (int j = 2; j <= x; j++)
+                    {
+                        sum *= j;
+                    }
+                    int unit_digit = sum % 10;
+                    int decimal_digit = (sum / 10) % 10;
+
+                    Console.WriteLine("{0} {1}", decimal_digit, unit_digit);
+                }
+                else Console.WriteLine("0 0");
+                
+            }
+        }
+    }
+}
