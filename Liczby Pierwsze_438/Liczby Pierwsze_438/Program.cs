16bc95096f0bbe69331da99e1860df281e2e3ac9
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-10 15:50:38 +0200

    done

---
+++ Liczby Pierwsze_438/Liczby Pierwsze_438/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Liczby_Pierwsze_438
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            n = Console.ReadLine();
+            for (int i = 0; i < int.Parse(n); i++)
+            {
+                string line;
+                line = Console.ReadLine();
+
+                int number = int.Parse(line);
+                if(number == 0 || number == 1) Console.WriteLine("NIE");
+                else if (IsPrime(number))
+                {
+                    Console.WriteLine("TAK");
+                }
+                else
+                {
+                    Console.WriteLine("NIE");
+                }
+            }
+        }
+        static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number + 1 / 2; i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+    }
+
+    
+}
+
