ac325b9e17126bcd24df89d5c3a1e976e92b7e4d
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-10 15:14:01 +0200

    done

---
+++ Samolot_1502/Samolot_1502/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Samolot_1502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] string_numbers = line.Split(' ');
+
+            int r1 = int.Parse(string_numbers[0]);
+            int s1 = int.Parse(string_numbers[1]);
+            int r2 = int.Parse(string_numbers[2]);
+            int s2 = int.Parse(string_numbers[3]);
+
+            Console.WriteLine("{0}", (r1 * s1) + (r2 * s2));
+            //Console.ReadLine();
+        }
+    }
+}
