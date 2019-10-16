3b7116acbfe27dccfa64fde79c1e1438abc6e13f
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-16 13:59:17 +0200

    start

---
+++ 606_Tablice/606_Tablice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _606_Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string data = Console.ReadLine();
+                string[] data_table = data.Split(' ');
+                string[] reverse_data_table = new string[data_table.Length - 1];
+                for (int j = 0; j < int.Parse(data_table[0]); j++)
+                {
+                    reverse_data_table[j] = data_table[data_table.Length - j];
+                }
+
+                Console.WriteLine(reverse_data_table);
+                
+            }
+        }
+    }
+}
