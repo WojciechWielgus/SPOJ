227f60c2512960e980821bd762f7d64a1771efc2
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-16 20:54:44 +0200

    done

--- 606_Tablice/606_Tablice/Program.cs
+++ 606_Tablice/606_Tablice/Program.cs
@@ -12,6 +12,7 @@ namespace _606_Tablice
                 string data = Console.ReadLine();
                 string[] data_table = data.Split(' ');
                 string[] reverse_data_table = new string[data_table.Length - 1];
+
                 for (int j = 0; j < int.Parse(data_table[0]); j++)
                 {
                     reverse_data_table[j] = data_table[data_table.Length - j - 1];
---
+++ 804_Gra Euklidesa/804_Gra Euklidesa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _804_Gra_Euklidesa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] players = Console.ReadLine().Split(' ');
+                int a = int.Parse(players[0]);
+                int b = int.Parse(players[1]);
+                Console.WriteLine(Game(a, b));
+            }
+        }
+
+        static int Game(int a, int b)
+        {
+            if (a > b)
+            {
+                if (a - b > 0) return Game(a - b, b);
+            }
+            if (b > a)
+            {
+                if (b - a > 0) return Game(a, b - a);
+            }
+            return a + b;
+        }
+
+    }
+}
