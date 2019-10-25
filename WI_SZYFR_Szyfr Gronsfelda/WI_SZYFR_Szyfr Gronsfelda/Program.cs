93fb4cf69f4987e7d960ad983029dfd3eeda1903
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-10-25 16:26:27 +0200

    done

--- WI_SZYFR_Szyfr Gronsfelda/WI_SZYFR_Szyfr Gronsfelda/Program.cs
+++ WI_SZYFR_Szyfr Gronsfelda/WI_SZYFR_Szyfr Gronsfelda/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WI_SZYFR_Szyfr_Gronsfelda
 {
@@ -12,46 +8,40 @@ namespace WI_SZYFR_Szyfr_Gronsfelda
 
         static void Main(string[] args)
         {
-            //string input;
-            //while((input = Console.ReadLine()) != null)
-            //{
-            //    string key = Console.ReadLine();
-            //    string data = Console.ReadLine();
-            //    switch (input)
-            //    {
-            //        case "SZYFRUJ":
-            //            Console.WriteLine(Szyfruj(data, key));
-            //            break;
-            //        default:
-            //            break;
-            //    }
-            //}
-
-            //while(true)
-            //{
-            //    Console.Write("Podaj klucz: ");
-            //    string key = Console.ReadLine();
-
-            //    Console.Write("Podaj znak: ");
-            //    string data = Console.ReadLine();
-
-            //    Console.WriteLine("After shift: " + SwapChar(data, Convert.ToInt32(key)));
-            //}
-
-            Console.WriteLine(Szyfruj("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "34"));
-            Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+                string key = Console.ReadLine();
+                string data = Console.ReadLine();
+                if(input == "SZYFRUJ") Console.WriteLine(Szyfruj(data, key));
+                else Console.WriteLine(Deszyfruj(data, key));
+            }
         }
 
         static string Szyfruj(string data, string key)
         {
             string new_word = "";
             char[] key_table = key.ToCharArray();
-            Console.WriteLine("key_table[0]: " + key_table[0]);
             int key_operator = 0;
             for (int i = 0; i < data.Length; i++)
             {
                 if (key_operator >= key_table.Length) key_operator = 0;
-                int key_temp =Convert.ToInt32(key_table[key_operator].ToString());              
+                int key_temp = Convert.ToInt32(key_table[key_operator].ToString());
+                new_word += SwapChar(data[i].ToString(), key_temp);
+                key_operator++;
+            }
+            return new_word;
+        }
+
+        static string Deszyfruj(string data, string key)
+        {
+            string new_word = "";
+            char[] key_table = key.ToCharArray();
+            int key_operator = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (key_operator >= key_table.Length) key_operator = 0;
+                int key_temp = -Convert.ToInt32(key_table[key_operator].ToString());
                 new_word += SwapChar(data[i].ToString(), key_temp);
                 key_operator++;
             }
@@ -61,9 +51,15 @@ namespace WI_SZYFR_Szyfr_Gronsfelda
         static string SwapChar(string currentChar, int shift)
         {
             for (int i = 0; i < alphabet.Length; i++)
-            {
-                if (alphabet[i] == currentChar) return alphabet[(i + shift) % alphabet.Length];               
-            }
+                if (alphabet[i] == currentChar)
+                {
+                    if (shift > 0) return alphabet[(i + shift) % alphabet.Length];
+                    else
+                    {
+                        if (i + shift < 0) return alphabet[alphabet.Length + shift];
+                        else return alphabet[i + shift];
+                    }
+                }
             return null;
         }
     }
